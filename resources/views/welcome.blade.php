88377c97fc4c178b81bc4732cbe41f85dc77906a
tamachine <tamara.rios@gmail.com>
2020-02-28 11:49:18 +0100

    otro cambio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: blue; /*#636b6f;*/
+                color: orange; /*#636b6f;*/
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
