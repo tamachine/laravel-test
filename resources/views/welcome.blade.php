cb09f31eb50624a80ad67a49854789c7351bfec4
tamachine <tamara.rios@gmail.com>
2020-02-28 23:02:46 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: blue; /*local 2*/
+                color: black; /*local test*/
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
